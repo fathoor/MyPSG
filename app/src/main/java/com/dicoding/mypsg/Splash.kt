9be2090a176bf25378aeaaf848ea7797f62b4716
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-03-01 19:14:38 +0700

    add: added splash screen

---
+++ app/src/main/java/com/dicoding/mypsg/Splash.kt
@@ -0,0 +1,21 @@
+package com.dicoding.mypsg
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, 2000)
+    }
+}
